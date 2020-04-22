11e86ad6f6f61987d933f5a4a9aec62801a370fb
L-cuitain <948527099@qq.com>
2020-04-22 22:00:11 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,6 +1,56 @@
-//鼠标移入导航栏列表某一项时 这项的颜色改变
-$('.nav-list-left').on({
+//鼠标移入导航栏列表某一项时 显示下拉列表
+$('.nav-content>ul>li').on({
     'mouseenter' : function(){
-        $(this).children('.nav-item-thead').animate({'backgroundColor':'#f2de05'},1000)
+        $('.nav-content>ul>li').children('ul').each(function(i){            
+            $(this).eq(i).hide();
+        })
+        
+        //获取ul中li的个数
+        let index =  $(this).children('ul').children('li').length;
+        //设置自动宽度
+        let autoHeight = 44 * index;
+        
+        $(this).children('ul').show().animate({width : 120 , height : autoHeight},700);
+    },
+    'mouseleave' : function(){
+        $(this).children('ul').animate({width : 0 , height : 0},100);
     }
-},'.nav-item') 
+}) 
+
+
+
+//精灵图动画
+$('.shift-item').on({
+    'mouseenter' : function(){
+        $('.shift').fadeOut(0,function(){
+            $('.shift-nosee').fadeIn(200);
+        });
+        
+    },
+    'mouseleave' : function(){
+        $('.shift-nosee').fadeOut(0,function(){
+            $('.shift').fadeIn(200);
+        });
+    },
+})
+
+
+$('.language-item').on({
+    'mouseenter' : function(){
+        $('.language').fadeOut(0,function(){
+            $('.language-nosee').fadeIn(200);
+        });
+        
+    },
+    'mouseleave' : function(){
+        $('.language-nosee').fadeOut(0,function(){
+            $('.language').fadeIn(200);
+        });
+    },
+})
+
+
+
+
+
+
